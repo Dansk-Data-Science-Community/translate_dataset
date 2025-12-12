fe763805322ccc73ffc36c13ac72b47cc5e2497a
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2025-12-12 20:30:13 +0100

    subset data and push to hub

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from translators import Translator, ColumnSpec
 import datasets
 
+# set to >0 to use only the first n_samples of the dataset
+n_samples = 10
+
 ds = datasets.load_dataset("microsoft/ms_marco", 'v1.1', split="train")
 
+if n_samples > 0:
+    ds = ds.select(range(n_samples))
 
 # How to use   
 cols_to_translate = [
@@ -22,4 +27,9 @@ translator = Translator(
 #ds = datasets.load_dataset("microsoft/ms_marco", 'v1.1', split="train")
 #load_dataset('microsoft/ms_marco', 'v1.1')`
 
+
 translated_ds = translator.translate(ds)
+
+translated_ds.push_to_hub("ThatsGroes/ms-marco-danish")
+
+translated_ds.to_csv("ms-marco-danish.csv")
